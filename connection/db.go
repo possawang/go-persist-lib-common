18ca5e34c4123812c67c7f5566a53cbc64c17f56
AchmadRifai <v-achmad.rifai@traveloka.com>
2022-04-18 05:14:38 +0700

    Global DB connection

--- connection/db.go
+++ connection/db.go
@@ -9,25 +9,26 @@ import (
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func ConnectionAndMigration(dst []interface{}) error {
-	db, err := Connecting()
+	err := connecting()
 	if err != nil {
 		return err
 	}
 	for _, i := range dst {
-		db.AutoMigrate(&i)
+		DB.AutoMigrate(&i)
 	}
 	return err
 }
 
-func Connecting() (*gorm.DB, error) {
-	var db *gorm.DB
+func connecting() error {
 	err := godotenv.Load(".env")
 	if err != nil {
-		return db, err
+		return err
 	}
 	user, pass, host, port, dbname := os.Getenv("DB.USER"), os.Getenv("DB.PASS"), os.Getenv("DB.HOST"), os.Getenv("DB.PORT"), os.Getenv("DB.NAME")
 	connectionStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, port, dbname)
-	db, err = gorm.Open(mysql.Open(connectionStr), &gorm.Config{})
-	return db, err
+	DB, err = gorm.Open(mysql.Open(connectionStr), &gorm.Config{})
+	return err
 }
